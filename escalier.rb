231d2c4c8873f9904617be40440bf7f76647a8f5
Elkantar-git <79974319+Elkantar-git@users.noreply.github.com>
2021-10-03 15:55:00 +0200

    Jeux de l'escalier Version Final

--- escalier.rb
+++ escalier.rb
@@ -1,21 +1,11 @@
 #VARIABLES
+
 nbr = gets.to_i
-list = ["   #", "  ##", " ###", "####"]
-listl = list.length
+list = [nil]
 
 #CODE
 
-puts list.take(nbr)
-
-#EVOL_CODE
-
-# Si nbr de list nil alors :
-# 1. indenter => list.map! {|x| " " + x} 
-# 2. rajouter "### * x"
-
-# if listl <= nbr
-#     puts list.take(nbr)
-# else
-#     list.push("#" * nbr)
-#     puts list.take(nbr)
-# end
+for num in 1..nbr
+    list.push(" " * (nbr - num) + ("#" * num)) 
+end
+puts list
