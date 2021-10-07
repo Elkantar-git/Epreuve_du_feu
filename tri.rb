c19c3407c11f1f7ccd44ad024ab82863ce031ad4
Elkantar-git <79974319+Elkantar-git@users.noreply.github.com>
2021-10-07 23:15:00 +0200

    tri.rb Version Finale 1 line

--- tri.rb
+++ tri.rb
@@ -1,18 +1,1 @@
-# VARIABLES
-
-ary = []
-
-
-# FONCTIONS
-
-
-
-# CODE
-
-for i in (0..4)
-    nbr = gets.to_i
-    ary.push(nbr)
-end
-
-ary.sort!.reverse!
-puts ary.inspect
+puts ARGV.map! {|x| x.to_i}.sort!.reverse!.join(" ")
